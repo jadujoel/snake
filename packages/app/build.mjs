1c465d478cb0fe2145b6dd8e7da51b9d1e3c651f
Joel Löf <jlof@evolution.com>
2023-10-20 20:11:45 +0200

    fix: remove unsupported watch in esbuild

--- packages/app/build.mjs
+++ packages/app/build.mjs
@@ -3,11 +3,7 @@ import { copy } from "esbuild-plugin-copy"
 import { wasmLoader } from 'esbuild-plugin-wasm';
 import { resolve } from "path";
 
-let args = process.argv.slice(2)
-let isWatch = args.includes("--watch")
-
 build({
-    watch: isWatch,
     entryPoints: [resolve("src", "app.ts")],
     outdir: resolve("dist"),
     bundle: true,
@@ -26,10 +22,6 @@ build({
                     from: resolve("src", "index.html"),
                     to: resolve("dist")
                 },
-                // {
-                //     from: resolve("src", "app.css"),
-                //     to: resolve("dist", "app.css")
-                // },
                 {
                     from: resolve("pkg", "app_bg.wasm"),
                     to: resolve("dist")
--- packages/app/src/app.ts
+++ packages/app/src/app.ts
@@ -1,6 +1,11 @@
 import init, { run_app } from '../pkg/app';
 import "./app.css";
 async function main() {
+    window.addEventListener('keydown', (ev) => {
+      if (ev.key === 'ArrowUp' || ev.key === 'ArrowDown') {
+         ev.preventDefault();
+       }
+    }, false)
    const here = new URL(window.location.href)
    await init(here.pathname + 'app_bg.wasm');
    run_app();
